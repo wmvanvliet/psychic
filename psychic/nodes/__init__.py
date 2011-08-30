234f143c1c80c75c38b4599902e90081f9d42cf0
Marijn van Vliet <w.m.vanvliet@student.utwente.nl>
2011-08-30 17:10:06 +0200

    Small fix

--- psychic/nodes/__init__.py
+++ psychic/nodes/__init__.py
@@ -1,6 +1,6 @@
 from spatialfilter import CAR, Whitening, SymWhitening, CSP, Deflate
 from timefreq import TFC
 from filter import Filter, OnlineFilter, Winsorize
-from window import SlidingWindow, OnlineSlidingWindow, OnlineSlice
-from wrapper import Decimate, Slice
+from window import SlidingWindow, OnlineSlidingWindow 
+from wrapper import Decimate, Slice, OnlineSlice
 from nonstat import SlowSphering
