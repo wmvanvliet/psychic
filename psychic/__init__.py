e98ce739b85a873d2ae1a0cc1318a1d2433f8c4a
Marijn van Vliet <marijn.vanvliet@med.kuleuven.be>
2012-07-04 11:18:38 +0200

    Added rereferencing and channel selection

--- psychic/__init__.py
+++ psychic/__init__.py
@@ -7,7 +7,7 @@ from utils import sliding_window_indices, sliding_window, stft, spectrogram,\
 from markers import markers_to_events, biosemi_find_ghost_markers, \
   resample_markers
 from plots import plot_timeseries, plot_scalpgrid, plot_eeg, plot_erp, plot_spectogram
-from filtering import filtfilt_rec, resample_rec, decimate_rec, ewma, ma
+from filtering import filtfilt_rec, resample_rec, decimate_rec, ewma, ma, rereference_rec, select_channels
 from parafac import parafac
 from expinfo import Experiment, ExperimentInfo, add_expinfo
 import nodes
--- psychic/filtering.py
+++ psychic/filtering.py
@@ -72,3 +72,76 @@ def decimate_rec(d, factor, max_marker_delay=0):
 
   # construct new DataSet
   return DataSet(xs=xs, ys=ys, ids=ids.reshape(-1, 1), default=d)
+
+def rereference_rec(d, reference_channels=None, keep=True):
+  '''
+  Re-reference a recording using the mean of the channels specified.
+  Optionally keep the channels used as a reference. Uses CAR by default.
+
+  Reference channels can be specified as a 1-D list, in which case
+  the same reference is applied to each channel.
+
+  Reference channels can also be specified as a 2-D list of lists that
+  speficies for each channel a list of reference channels.
+
+  Reference channels can either be integer indexes or string feature labels in
+  which case a lookup in the feat_lab of the dataset is performed.
+  '''
+  nchannels = d.nfeatures
+
+  # Use common average referencing by default
+  if reference_channels == None:
+    reference_channels = range(nchannels)
+
+  # Check if given list of lists and convert feature labels to channel indices
+  list_of_lists = None
+  for i,ref in enumerate(reference_channels):
+    if type(ref) == list:
+      if list_of_lists == False:
+        raise RuntimeError('Expected reference channels to be either a list'
+                           'of integers or a list of lists.')
+
+      list_of_lists = True
+      for j,l in enumerate(ref):
+        if type(l) == str:
+          reference_channels[i][j] = d.feat_lab.index(l)
+    else:
+      if list_of_lists == True:
+        raise RuntimeError('Expected reference channels to be either a list'
+                           'of integers or a list of lists.')
+
+      list_of_lists = False
+      if type(ref) == str:
+        reference_channels[i] = d.feat_lab.index(ref)
+
+  if list_of_lists:
+    X = d.X.copy()
+    for i in range(nchannels):
+      X[i,:] = X[i,:] - np.mean(X[reference_channels[i], :], axis=0)
+  else:
+    X = d.X - np.tile(np.mean(d.X[reference_channels,:], axis=0), (nchannels,1))
+
+  if not keep:
+    X = X[np.array([not c in reference_channels for c in range(nchannels)], dtype=np.bool), :]
+    feat_lab = [l for i,l in enumerate(d.feat_lab) if not i in reference_channels]
+  else:
+    feat_lab = d.feat_lab
+
+  return DataSet(X=X, feat_shape=(X.shape[0],), feat_lab=feat_lab, default=d)
+
+def select_channels(d, channels):
+  '''
+  Select subset of channels from a recording.
+  Channels can be specified either as a list of integers, a list of feature
+  labels or a list of bools.
+  '''
+  for i,l in enumerate(channels):
+    if type(l) == str:
+      channels[i] = d.feat_lab.index(l)
+
+  X = d.X[np.asarray(channels), :]
+  feat_shape = (X.shape[0],)
+  feat_lab = [l for i,l in enumerate(d.feat_lab) if i in channels]
+
+  return DataSet(X=X, feat_lab=feat_lab, feat_shape=feat_shape, default=d)
+
