d90b5923e6c8d11f980d648ff90155fbc4fe01fd
Marijn van Vliet <marijn.vanvliet@med.kuleuven.be>
2013-02-14 10:36:49 +0100

    Update module details

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='psychic',
-  version='0.1',
+  version='0.3',
   license='GPL',
   url='http://www.borisreuderink.nl',
-  author='Boris Reuderink',
-  author_email='b.reuderink@gmail.com',
+  author='Boris Reuderink, Marijn van Vliet',
+  author_email='b.reuderink@gmail.com, w.m.vanvliet@gmail.com',
   packages=['psychic', 'psychic.nodes', 'psychic.tests', 'psychic.dataformats'],
   )
