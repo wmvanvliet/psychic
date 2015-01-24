78eae3db3efc6976f2fe10ad15379ee70caa2c3e
Marijn van Vliet <w.m.vanvliet@gmail.com>
2015-01-24 12:56:55 +0200

    beamformer improvements
    Former-commit-id: b26b58e6bcdcc81afcf1637d71ecba823d994632

--- psychic/nodes/beamformer.py
+++ psychic/nodes/beamformer.py
@@ -1,106 +1,130 @@
 from basenode import BaseNode
 from ..dataset import DataSet
+from ..trials import baseline, concatenate_trials
 import numpy as np
 from ..stat import lw_cov
 from psychic.nodes.spatialfilter import SpatialFilter
-
+from sklearn.grid_search import GridSearchCV
+from sklearn.covariance import ShrunkCovariance
 
 class SpatialBeamformer(SpatialFilter):
     '''
-    Spatial beamformer.
+    LCMV Spatial beamformer.
 
     Parameters
     ----------
     template : 1D array (n_channels)
        Spatial activation pattern of the component to extract.
 
-    reg : float (default: 0.2)
+    reg : float (default: 1e-5)
         Regularization parameter for the covariance matrix inversion. Also
         known as diagonal loading.
 
     cov_func : function (default: lw_cov)
         Covariance function to use. Defaults to Ledoit & Wolf's function.
     '''
-    def __init__(self, template, reg=0.2, cov_func=lw_cov, lcmv=False):
+    def __init__(self, template, reg=None, cov_func=lw_cov, normalize=True):
         SpatialFilter.__init__(self, 1)
         self.template = template
         self.reg = reg
         self.cov_func = cov_func
         self.template = np.asarray(template).flatten()[:, np.newaxis]
-        self.lcmv = lcmv
+        self.normalize = normalize
 
-    def center(self, d):
-        """Center the data in time"""
-        return d
+        if normalize:
+            self.template -= self.template.mean()
 
     def train_(self, d):
-        d = self.center(d)
+        if self.normalize:
+            d = DataSet(d.data - d.data.mean(axis=0), default=d)
+            d = baseline(d)
 
         # Calculate spatial covariance matrix
-        sigma_x = np.mean(
-            [self.cov_func(t) for t in np.rollaxis(d.data, -1)],
-            axis=0)
-        sigma_x += self.reg * np.eye(sigma_x.shape[0])
-        sigma_x_i = np.linalg.inv(sigma_x)
+        if self.reg is None:
+            gs = GridSearchCV(ShrunkCovariance(assume_centered=True), [{'shrinkage': np.logspace(-5, 0, 6)}])
+            c = gs.fit(concatenate_trials(d).X).best_estimator_
+            print 'Estimated shrinkage: ', c.shrinkage
+        else:
+            c = ShrunkCovariance(assume_centered=True, shrinkage=self.reg).fit(concatenate_trials(d).X)
+        sigma_x_i = c.precision_
+        
+        #sigma_x = np.mean(
+        #    [self.cov_func(t) for t in np.rollaxis(d.data, -1)],
+        #    axis=0) / (len(d) - 1)
+        #sigma_x += self.reg * np.eye(sigma_x.shape[0])
+        #sigma_x_i = np.linalg.inv(sigma_x)
         self.W = sigma_x_i.dot(self.template)
-        if self.lcmv:
-            self.W = self.W.dot(
-                reduce(np.dot, self.template.T, sigma_x_i, self.template)
-            )
+
+        # Noise normalization
+        self.W = self.W.dot(
+            np.linalg.inv(reduce(np.dot, [self.template.T, sigma_x_i, self.template]))
+        )
 
     def apply_(self, d):
-        d = self.center(d)
+        d = DataSet(d.data - d.data.mean(axis=0), default=d)
+        d = baseline(d)
         return SpatialFilter.apply_(self, d)
 
 
 class TemplateBeamformer(BaseNode):
     '''
-    Beamformer operating on a template.
+    LCMV Beamformer operating on a template.
 
     Parameters
     ----------
     template : 2D array (n_channels, n_samples)
        Activation pattern of the component to extract.
 
-    reg : float (default: 0.2)
+    reg : float (default: 1e-5)
         Regularization parameter for the covariance matrix inversion. Also
         known as diagonal loading.
 
     cov_func : function (default: lw_cov)
         Covariance function to use. Defaults to Ledoit & Wolf's function.
     '''
-    def __init__(self, template, reg=0.2, cov_func=lw_cov, lcmv=False):
+    def __init__(self, template, reg=None, cov_func=lw_cov, normalize=True):
         BaseNode.__init__(self)
         self.template = template
         self.reg = reg
         self.cov_func = cov_func
         self.template = np.atleast_2d(template)
-        self.lcmv = lcmv
+        self.normalize = normalize
 
     def center(self, d):
-        """Center the data in time"""
-        return d
+        data_mean = d.data.reshape(-1, len(d)).mean(axis=1)
+        data_mean = data_mean.reshape(d.feat_shape + (1,))
+        return DataSet(d.data - data_mean, default=d)
 
     def train_(self, d):
-        d = self.center(d)
+        if self.normalize:
+            d = self.center(d)
 
         nsamples, ntrials = d.data.shape[1:]
         template = self.template[:, :nsamples]
-        sigma_x = self.cov_func(d.data.reshape(-1, ntrials))
-        sigma_x += self.reg * np.eye(sigma_x.shape[0])
-        sigma_x_i = np.linalg.inv(sigma_x)
+        #sigma_x = self.cov_func(d.data.reshape(-1, ntrials))
+        #sigma_x += self.reg * np.eye(sigma_x.shape[0])
+        #sigma_x_i = np.linalg.inv(sigma_x)
+
+        if self.reg is None:
+            gs = GridSearchCV(ShrunkCovariance(assume_centered=True), [{'shrinkage': np.logspace(-5, 0, 6)}])
+            c = gs.fit(d.data.reshape(-1, ntrials).T).best_estimator_
+            print 'Estimated shrinkage: ', c.shrinkage
+        else:
+            c = ShrunkCovariance(assume_centered=True, shrinkage=self.reg).fit(d.data.reshape(-1, ntrials).T)
+        sigma_x_i = c.precision_
 
         template = self.template.flatten()[:, np.newaxis]
-        self.W = sigma_x_i.dot(template).ravel()
+        self.W = sigma_x_i.dot(template)
 
-        if self.lcmv:
-            self.W = self.W.dot(
-                reduce(np.dot, self.template.T, sigma_x_i, self.template)
-            )
+        # Noise normalization
+        self.W = self.W.dot(
+            np.linalg.inv(reduce(np.dot, [template.T, sigma_x_i, template]))
+        )
 
     def apply_(self, d):
-        d = self.center(d)
+        if self.normalize:
+            d = self.center(d)
 
         ntrials = d.data.shape[2]
-        X = self.W.dot(d.data.reshape(-1, ntrials))
+        X = self.W.T.dot(d.data.reshape(-1, ntrials))
         return DataSet(data=X, feat_lab=None, default=d)
