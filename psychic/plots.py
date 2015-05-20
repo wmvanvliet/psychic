a3131635908fa7fcc16c1e322a6d3fc248da0ee1
Marijn van Vliet <w.m.vanvliet@gmail.com>
2015-05-20 15:15:20 +0200

    doc updates
    Former-commit-id: cbac689999874d9a0e2a57731535d4a6bc76d5d2

--- psychic/plots.py
+++ psychic/plots.py
@@ -611,18 +611,12 @@ def plot_erp_specgrams(
         fig = plot.figure()
 
     tf = trials.trial_specgram(d, samplerate, NFFT)
-    print tf.d.shape
     tf_erp = np.mean(tf.d, axis=3)
-    print tf_erp.shape
     
     ch_labs = tf.feat_lab[0]
-    print ch_labs
 
     freqs = np.array([float(x) for x in tf.feat_lab[1]])
     times = np.array([float(x) for x in tf.feat_lab[2]])
-
-    print freqs
-    print times
 
     selection = np.logical_and(freqs >= freq_range[0], freqs <= freq_range[1])
     freqs = freqs[selection]
@@ -635,18 +629,6 @@ def plot_erp_specgrams(
 
     fig.subplots_adjust(hspace=0)
 
-    #cdict = {'red': ((0.0, 1.0, 1.0),
-    #                 (0.5, 1.0, 1.0),
-    #                 (1.0, 0.0, 0.0)),
-    #         'green': ((0.0, 0.0, 0.0),
-    #                   (0.5, 1.0, 1.0),
-    #                   (1.0, 0.0, 0.0)),
-    #         'blue': ((0.0, 0.0, 0.0),
-    #                  (0.5, 1.0, 1.0),
-    #                  (1.0, 1.0, 1.0))}
-
-    #cmap = matplotlib.colors.LinearSegmentedColormap('polarity',cdict,256)
-
     for channel in range(num_channels):
         s = tf_erp[channel,:,:]
 
@@ -656,7 +638,6 @@ def plot_erp_specgrams(
         im = plot.imshow(
             np.flipud(s), aspect='auto',
             extent=[np.min(times), np.max(times), np.min(freqs), np.max(freqs)],
-            #cmap=cmap
         )
 
         plot.ylim(freq_range[0], freq_range[1])
@@ -1070,7 +1051,6 @@ def plot_topo(
         plt.ylabel(u'voltage (µV)')
 
     for ch, ax in enumerate(layout.axes(fig)):
-        print ch
         plot_channel(ax, ch)
 
     if draw_scale:
